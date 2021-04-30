d2d06d6565ec78f66838eddfa9398a811da26e86
huzr3 <huzr3@midea.com>
2021-04-30 17:04:14 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+    es6: true
+  },
+  ignorePatterns: ["example/**/*", ".eslintrc.js"],
+  rules: {
+    "linebreak-style": 0,
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-this-alias": [
+      "error",
+      {
+        allowDestructuring: true,
+        allowedNames: ["self"]
+      }
+    ]
+  }
+};
---
+++ README.md
@@ -0,0 +1,15 @@
+# Description
+
+A sdk for wx miniprogramme error and performance monitor.
+
+# Development
+
+```
+npm run watch
+```
+
+# Build
+
+```
+npm run build
+```
